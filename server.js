588435a43e9b6463662d8f99e0f263d1eed81641
ndallich1 <ndallich@gmail.com>
2023-04-02 13:48:31 -0400

    created connection & installed create.table

--- server.js
+++ server.js
@@ -6,6 +6,27 @@ const prompts = require("./lib/prompts");
 const Engineer = require("./lib/department");
 const Intern = require("./lib/employee");
 const Manager = require("./lib/role");
+const cTable = require("console.table");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: "SQL4me1!",
+    database: "classlist_db",
+  },
+  console.log(`Connected to the classlist_db database.`)
+);
 
 // create connection to database
 const db = mysql.createConnection(
