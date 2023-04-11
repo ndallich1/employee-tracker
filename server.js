b776d57eb9eb932c10c0230832aca33118bc4f26
ndallich1 <ndallich@gmail.com>
2023-04-10 20:32:09 -0400

    updated server.js and prompts.js

--- server.js
+++ server.js
@@ -2,33 +2,13 @@
 const inquirer = require("inquirer");
 // import and require mysql2
 const mysql = require("mysql2");
-const prompts = require("./lib/prompts");
+const { menu, addDepartment } = require("./lib/prompts");
 const Engineer = require("./lib/department");
 const Intern = require("./lib/employee");
 const Manager = require("./lib/role");
 const cTable = require("console.table");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Connect to database
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    // MySQL username,
-    user: "root",
-    // MySQL password
-    password: "SQL4me1!",
-    database: "classlist_db",
-  },
-  console.log(`Connected to the classlist_db database.`)
-);
-
-// create connection to database
+// connect to database
 const db = mysql.createConnection(
   {
     host: "localhost",
@@ -39,3 +19,69 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the employee_db database.`)
 );
+
+const initialQuestion = () => {
+  inquirer.prompt(menu).then((answers) => {
+    chosenTask(answers);
+  });
+};
+
+initialQuestion();
+
+const chosenTask = ({ options }) => {
+  switch (options) {
+    case "View all departments":
+      viewDepartments();
+      break;
+    case "View all employees":
+      viewEmployees();
+      break;
+    case "View all roles":
+      viewRoles();
+      break;
+    case "Add a department":
+      addDepartment();
+      break;
+    case "Add an employee":
+      addEmployee();
+      break;
+    case "Add a role":
+      addRole();
+      break;
+    case "Update an employee role":
+      updateEmpRole();
+      break;
+    case "Quit":
+      quit();
+      break;
+  }
+};
+
+// const viewDepartments = () => {
+//   db.query (
+//     `SELECT
+//     department.name AS 'Department',
+//     department.id AS 'Department ID'
+//     FROM
+//     department`,
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.table(results);
+//   )
+// };
+
+function addDepartment() {
+  inquirer.prompt(addDepartment).then((answers) => {
+    db.query(
+      `INSERT INTO department (name)
+      VALUES (?)`,
+      [answers.deptName],
+      console.log("Added new department")
+    );
+    if (err) {
+      console.log(err);
+    }
+    console.table(results);
+  });
+}
